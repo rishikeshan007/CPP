a346d4a58e6dde375c7eac435be6175dd45841a6
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2021-09-05 16:47:24 +0530

    Create solid rectangular.cpp

---
+++ pattern matching/solid rectangular.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"enter r"<<endl;
+    cin>>r;
+    int c;
+    cout<<"enter c"<<endl;
+    cin>>c;
+    for(int i=0;i<=r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+//output if r=3 and c=5
+//*****
+//*****
+//*****
