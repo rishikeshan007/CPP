f2da35788ba1da0ed3a6178d27a8ab4e3f9ac07a
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2021-09-12 08:45:37 +0530

    Create Deletion at specific.cpp

---
+++ Arrays/Deletion/Deletion at specific.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[50];
+    int n;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    cout<<"Enter the array"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int pos;
+    cout<<"enter the position to delete"<<endl;
+    cin>>pos;
+    if((pos<=0)||(pos>n))
+    {
+        cout<<"Invalid"<<endl;
+    }
+    else
+    {
+        for(int i=pos-1;i<n-1;i++)
+        {
+            arr[i]=arr[i+1];
+            
+        }
+        n--;
+    }
+    
+    cout<<"the resultant array is"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
